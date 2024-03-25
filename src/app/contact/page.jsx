968b4bc84b8d70d75722c1e99500f1d0131c40ed
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-03-25 20:30:33 +0330

    ...

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./contact.module.css";
 
 export default function ContactPage() {
-  return <div className="container">ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/flower2.png" alt="" fill />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.formContainer}>
+          <input type="text" className={styles.input} placeholder="" />
+        </form>
+      </div>
+    </div>
+  );
 }
