c63e02b941a7a7542a19b17ca8383a66fd16aa88
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-03-26 13:55:59 +0330

    create post and single post

--- learn-next-lamadev/src/app/blug/[Slug]/page.jsx
+++ learn-next-lamadev/src/app/blug/[Slug]/page.jsx
@@ -1,5 +1,43 @@
 import React from "react";
+import styles from "./singlePost.module.css";
+import Image from "next/image";
 
-export default function SinglePagePost() {
-  return <div>SinglePagePost</div>;
+export default function SinglePostPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/flower2.png"
+          alt="single post image"
+          className={styles.img}
+          fill
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}></h1>
+        <div className={styles.detail}>
+          <Image
+            src="/flower2.png"
+            width={50}
+            height={50}
+            className={styles.avatar}
+            alt="single image"
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Date</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae,
+          cupiditate quos! Nesciunt voluptas, autem eveniet quod sit est
+          voluptatum dolor?
+        </div>
+      </div>
+    </div>
+  );
 }
