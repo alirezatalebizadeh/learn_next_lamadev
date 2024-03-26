f5d173fe73930ed20bf974f82f76097d13beab63
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-03-26 12:30:04 +0330

    create contact design

--- learn-next-lamadev/src/app/contact/page.jsx
+++ learn-next-lamadev/src/app/contact/page.jsx
@@ -5,11 +5,26 @@ export default function ContactPage() {
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image src="/flower2.png" alt="" fill />
+        <Image src="/flower2.png" className={styles.img} alt="" fill />
       </div>
       <div className={styles.formContainer}>
-        <form action="" className={styles.formContainer}>
-          <input type="text" className={styles.input} placeholder="" />
+        <form action="" className={styles.form}>
+          <input
+            type="text"
+            className={styles.input}
+            placeholder="Name and SurName"
+          />
+          <input
+            type="text"
+            className={styles.input}
+            placeholder="Email Address"
+          />
+          <input
+            type="text"
+            className={styles.input}
+            placeholder="Phone Number (optional)"
+          />
+          <textarea name="" id="" placeholder="Message"></textarea>
         </form>
       </div>
     </div>
